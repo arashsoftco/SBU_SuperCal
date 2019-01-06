54c938ba58e8d9563c3d540065dff61d4a725eb5
آرش رضایی <arashsoft@outlook.com>
2019-01-06 15:05:45 +0330

    Update Digit_Op_Encoder.ino

--- Digit_Op_Encoder.ino
+++ Digit_Op_Encoder.ino
@@ -1,6 +1,6 @@
 #define one 14  
-#define two 13  
-#define three 12  
+#define two 7  
+#define three 13  
 #define four 11
 #define five 10
 #define six 16
@@ -12,31 +12,41 @@
 #define sent 15
 
 
+//// ADC ERROR
+
 void setup()
 {
-pinMode(one,INPUT_PULLUP);
-pinMode(two,INPUT_PULLUP);
-pinMode(three,INPUT_PULLUP);
-pinMode(four,INPUT_PULLUP);
-pinMode(five,INPUT_PULLUP);
-pinMode(six,INPUT_PULLUP);
-pinMode(seven,INPUT_PULLUP);
-pinMode(eight,INPUT_PULLUP);
-pinMode(nine,INPUT_PULLUP);
-pinMode(zero,INPUT_PULLUP);
+  
+pinMode(one,INPUT);
+pinMode(two,INPUT);
+pinMode(three,INPUT);
+pinMode(four,INPUT);
+pinMode(five,INPUT);
+pinMode(six,INPUT);
+pinMode(seven,INPUT);
+pinMode(eight,INPUT);
+pinMode(nine,INPUT);
+pinMode(zero,INPUT);
+pinMode(sent,OUTPUT);
 
 }
 
 void PWM_digit(int sig)  /// ADC A2
 {
-  analogWrite(sender_digit,sig);
+  sig=100;
+ analogWrite(sender_digit,sig);
   digitalWrite(sent,1);
-  delay(100);
+ 
+  delay(20);
   digitalWrite(sent,0);
+  //
+
+  
 }
 
 void loop() 
 {
+  //PWM_digit(100);
   
  int r1 = digitalRead(one);
  int r2 = digitalRead(two);
@@ -51,15 +61,26 @@ void loop()
 
 
 if(r1==1) PWM_digit(100);
-if(r2==1) PWM_digit(200);
+else
+if(r2==1) PWM_digit(100);
+else
 if(r3==1) PWM_digit(300);
+else
 if(r4==1) PWM_digit(400);
+else
 if(r5==1) PWM_digit(500);
+else
 if(r6==1) PWM_digit(600);
+else
 if(r7==1) PWM_digit(700);
+else
 if(r8==1) PWM_digit(800);
+else
 if(r9==1) PWM_digit(900);
+else
 if(r0==1) PWM_digit(1000);
+else
+ analogWrite(sender_digit,0);
 
 
 
