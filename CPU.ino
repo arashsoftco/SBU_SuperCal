d75f93e12e0c96905e8ee7ccf4ce3c6260e02cab
آرش رضایی <arashsoft@outlook.com>
2019-01-17 17:01:16 +0330

    Create CPU.ino

---
+++ CPU.ino
@@ -0,0 +1,187 @@
+/**************************************************************************
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64 
+
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+
+void receiveEvent(int howMany)
+
+{
+  while (1 < Wire.available()) 
+  {
+    char c = Wire.read(); 
+    Serial.print(c);        
+  }
+  int x = Wire.read();
+  Serial.println(x);         
+  Drawi(x);
+
+  
+}
+
+
+void setup() {
+    Wire.begin(8);                // join i2c bus with address #8
+        
+  
+  Serial.begin(9600);
+
+ 
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+
+  
+
+
+  display.clearDisplay();
+
+
+SCroll();
+
+Intro(); 
+
+
+  Wire.onReceive(receiveEvent);
+}
+
+void loop()
+{
+ delay(100); 
+}
+
+
+
+String c="";
+
+void Drawi(int a) 
+{
+  display.clearDisplay();
+
+  display.setTextSize(1);     
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);    
+  display.cp437(true);        
+
+
+if (a>=10)
+{  
+  
+  display.setTextSize(8); 
+  
+  switch(a)
+{
+  case(11):
+     display.print(" + ");
+     break;
+  
+   
+  case(12):
+     display.print(" - ");
+     break;
+  
+   
+  case(13):
+     display.print(" X ");
+     break;
+
+     case(14):
+     display.print(" / ");
+     break;
+
+     
+     case(18):
+    {
+      display.print("0x88");
+       break;
+    }
+    
+     
+     case(19):
+     {
+        display.setCursor(10, 10); 
+        display.setTextSize(3); 
+      display.print("A ^ B");
+     break;
+     }
+     
+     
+}
+
+}
+else
+{
+   c+=String(a);
+   
+ display.print(c);
+
+
+} 
+
+display.display();
+    delay(2000);
+}
+
+void Intro(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);         
+
+  display.setCursor(0,0);            
+  display.println(F("Arashsoft EAI"));
+
+ display.println(" ");
+
+  display.setTextSize(2);             
+  display.setTextColor(WHITE);
+  display.print("SBU");
+ display.println("  ");
+ 
+  display.setTextSize(2);            
+  display.setTextColor(WHITE);
+  display.println("Super Cal :)");
+  
+  display.display();
+  delay(2000);
+}
+
+void SCroll()
+{
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(10, 0);
+  display.println(F("Arashsoft EAI"));
+  display.display();    
+  delay(100);
+
+
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+}
